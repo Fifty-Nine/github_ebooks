aff9c4996042e2f831315c844f35a92591939ace
Fifty-Nine <tim.prince@gmail.com>
2013-08-21 18:15:44 -0500

    Add --generate option.

--- github_ebooks.py
+++ github_ebooks.py
@@ -4,6 +4,7 @@ import argparse
 import codecs
 
 from Database import Database
+from Markov import SequenceGenerator
 
 def readFromFile(path, db):
   f = codecs.open(path, 'r', 'utf-8')
@@ -19,6 +20,14 @@ def printCommits(db):
   for (hash, msg) in db.allCommits():
     print msg
 
+def generate(db):
+  sg = SequenceGenerator(1)
+  for commit in db.allCommits():
+    sg.addSample(commit[1].split(' '))
+
+  print ' '.join(sg.generate())
+
+
 def main(argv):
   parser = argparse.ArgumentParser(description='github_ebooks')
   parser.add_argument('--api-key', dest='api_key',
@@ -26,6 +35,8 @@ def main(argv):
   parser.add_argument('--commit-file', dest='commit_file', 
       help='Read commits from the given file and save them in the database')
   parser.add_argument('--print-commits', dest='print_commits', 
+      action='store_const', const=True, default=False)
+  parser.add_argument('--generate', dest='generate', 
       action='store_const', const=True, default=False)
   args = parser.parse_args(argv[1:])
 
@@ -39,6 +50,9 @@ def main(argv):
 
   if args.print_commits:
     printCommits(db)
+
+  if args.generate:
+    generate(db)
 
   return 0
 
