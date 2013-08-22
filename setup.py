586cafaede3415ccfae543b3e7778d5b883b13d2
Fifty-Nine <tim.prince@gmail.com>
2013-08-22 18:02:28 -0500

    Add setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='github_ebooks', 
+      version='0.1',
+      description='GitHub Commit Generator',
+      author='Tim Prince',
+      author_email='tim.prince@gmail.com',
+      url='http://github.com/Fifty-Nine/github_ebooks',
+      packages=['github_ebooks']
+      )
+
